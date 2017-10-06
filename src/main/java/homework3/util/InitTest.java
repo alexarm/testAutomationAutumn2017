454ef40c838f482957dd41c58de45334f367a388
alexarm <alex.armenshin@gmail.com>
2017-10-06 13:18:37 +0300

    add allure

--- src/main/java/homework3/util/InitTest.java
+++ src/main/java/homework3/util/InitTest.java
@@ -14,7 +14,7 @@ public abstract class InitTest {
 
     public InitTest() {
         Configuration.browser = "Chrome";
-        Configuration.startMaximized = true;
+        Configuration.browserSize = "1440x900";
         Configuration.baseUrl = "https://jdi-framework.github.io/tests";
     }
 
